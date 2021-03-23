97ec0a11562e38ddec7260ef011474c332f424e4
Shivani Wadhwa <Shivani.Wadhwa@condecosoftware.com>
2021-03-24 00:23:58 +0530

    Logging added.

---
+++ Workspace/Workspace.Infrastucture/Service/Logger/DependencyInjection.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Serilog;
+using Workspace.Application.Common.Interfaces;
+
+namespace Workspace.Infrastucture.Service.Logger
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddLogger(this IServiceCollection serviceDescriptors, IConfiguration configuration)
+        {
+            Log.Logger = new LoggerConfiguration().ReadFrom.Configuration(configuration).CreateLogger();
+            serviceDescriptors.AddSingleton(typeof(ILoggerAdapter<>), typeof(Logger<>));
+            serviceDescriptors.AddSingleton<ILogger>(Log.Logger);
+            return serviceDescriptors;
+        }
+    }
+}
---
+++ Workspace/Workspace.Infrastucture/Service/Logger/Logger.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Serilog;
+using Workspace.Application.Common.Interfaces;
+
+namespace Workspace.Infrastucture.Service.Logger
+{
+    public class Logger<T>: ILoggerAdapter<T>
+    {
+        private readonly ILogger _logger;
+
+        public Logger(ILogger logger)
+        {
+            _logger = logger;
+        }
+
+        public void LogInformation(string message)
+        {
+            _logger.Information(message);
+        }
+
+        public void LogDebug(string message)
+        {
+            _logger.Debug(message);
+        }
+
+        public void LogError(Exception ex, string message, params object[] args)
+        {
+            _logger.Error(ex, message, args);
+        }
+    }
+}
